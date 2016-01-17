c2ebe4c7294a5eb5428b115ca211f94976cd8dff
Ernest D. Peterson <petesplace312@yahoo.com>
2016-01-16 18:39:55 -0600

    Commit first save of java course

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,13 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
- */
+ var awesomeThoughts = "I am Ernest and I am AWSOME!";
+ console.log(awesomeThoughts);
+var funThoughts = awesomeThoughts.replace("AWSOME", "FUN");
+ $("#main").append(funThoughts);
+  */
+  var formattedName = "Ernest D. Petereson";
+  var formattedRole = "Web Developer";
+  var Role = HTMLheaderRole.replace("%data%", formattedRole);
+  var Name = HTMLheaderName.replace("%data%", formattedName);
+  $("#header").prepend(Role);
+  $("#header").prepend(Name);
