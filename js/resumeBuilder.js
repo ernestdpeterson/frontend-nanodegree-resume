2ba623873819d313af3b6bfd28a76cb6e40b2d3a
Ernest D. Peterson <petesplace312@yahoo.com>
2016-01-24 20:23:28 -0600

    Start Lesson 2

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,48 +1,96 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- var awesomeThoughts = "I am Ernest and I am AWSOME!";
- console.log(awesomeThoughts);
-var funThoughts = awesomeThoughts.replace("AWSOME", "FUN"); 
- var A = ["sponge", "salt", "bacon", "pizza"] 
- var A = ["sponge salt", "bacon", "pizza"];
- var space = " ";
- var Aa = A[0];
- var Ab = Aa.slice(1);
- var Ac = Aa[0];
- var Ad = (Ac.toUpperCase() + Ab);
- var Ae = Aa.split(space)*/
- /*$("#main").append(A.length);*/
-/*$("#main").append(Ae[0]);*/
-var formattedName = "Ernest D. Petereson";
-var formattedRole = "Web Developer";
-var Role = HTMLheaderRole.replace("%data%", formattedRole);
-var Name = HTMLheaderName.replace("%data%", formattedName);
-$("#header").prepend(Role);
-$("#header").prepend(Name);
 var bio = {
     "FirstName" : "Ernest",
     "LastName" : "Peterson",
     "MiddleName" : "D.",
     "Role" : "Web Developer",
-    "ContactCell" : "Cell 601-307-4377",
-    "ContactEmail" : "petesplace312@yahoo.com",
+    "Contacts" : [
+        "Cell 601-307-4377",
+        "E-Mail petesplace312@yahoo.com",
+        "GitHub ernestdpeterson"
+    ],
     "PictureURL" : "images/smiley.gif",
     "Location" : "Pensacola, FL",
     "WelcomeMessage" : "Welcome To My Resume Page",
-    "Skills" : ["SQL", "Truck Driver", "Carpentry", "Electrician", "Computer Repair", 
-        "HTML & CSS"]
-};
-var space = " ";
-var wmessage = HTMLwelcomeMsg.replace("%data%", bio.WelcomeMessage);
-var bodyname = HTMLcontactGeneric.replace("%contact%", bio.FirstName + space + 
-    bio.MiddleName + space + bio.LastName).replace("%data%", bio.Role);
-var mainimage = HTMLbioPic.replace("%data%", bio.PictureURL);
-var email = HTMLemail.replace("%data%", bio.ContactEmail);
-var cell = HTMLmobile.replace("%data%", bio.ContactCell);
-var locality = HTMLlocation.replace("%data%", bio.Location)
-$("#header").append(mainimage);
-$("#header").append(wmessage);
-$("#header").append(bodyname);
-$("#header").append(email);
-$("#header").append(cell);
-$("#header").append(locality);
+    /*"Skills" : ["SQL", "Truck Driver", "Carpentry", "Electrician", "Computer Repair", 
+        "HTML & CSS", "Mechanic", "Welder", "Forklift Operator"]*/
+}
+var work = {
+    "jobs" : [
+        {
+        "employer" : "Airgas/Dry Ice",
+        "title" : "Truck Driver",
+        "location" : ["Star, MS", "Deer Park, TX"],
+        "dates" : ["06/2008", "Current"],
+        "description" : "Loading, Delivering and unloading carbon dioxide."
+        },
+        {
+        "employer" : "Self Employed",
+        "title" : "Truck Driver",
+        "location" : "Seminary, MS",
+        "dates" : ["04/2003", "06/2008"],
+        "description" : "Owner and Operator of a small trucking company."
+        },
+        {
+        "employer" : "Flying J",
+        "title" : "Truck Driver",
+        "location" : "Perl, MS",
+        "dates" : ["10/1999", "04/2003"],
+        "description" : "Fuel Tanker Driver"
+        },
+        {
+        "employer" : "Fleet Morris",
+        "title" : "Truck Driver",
+        "location" : "Collins, MS",
+        "dates" : ["05/1997", "10/1999"],
+        "description" : "Fuel Tanker Driver."
+        },
+        {
+        "employer" : "Sanderson Farms",
+        "title" : "Truck Driver/Manager",
+        "location" : "Collins, MS",
+        "dates" : ["11/1993", "05/1997"],
+        "description" : "Managed a spray crew and drove a live haul truck."
+        },
+        {
+        "employer" : "T&L Trucking",
+        "title" : "Truck Driver",
+        "location" : "Big Creek, MS",
+        "dates" : ["02/1991", "11/1993"],
+        "description" : "Long haul truck driver."
+        }
+    ]
+}
+
+var education = {
+    "schools" : [
+        {
+            "name" : "O'Reilly School of Technology",
+            "city" : "Online",
+            "degree" : "N/A",
+            "major" : "Introduction to Relational Databases"
+        },
+        {
+            "name" : "University of Pheonix",
+            "city" : "Online",
+            "degree" : "N/A",
+            "major" : "CompTIA +"
+        },
+        {
+            "name" : "Udacity",
+            "city" : "Online",
+            "degree" : "N/A",
+            "major" : "Front End Web Developer" 
+        }
+    ]
+}
+var projects = {
+    "Resume" : [
+        "title" : "Udacity Resume Project".
+        "dates" : ["01/2016", "Current"],
+        "description" : "Build a resume using HTML, CSS and Java."
+    ]
+}
+// Formatting from helper.js past this line
+
+// Append to index.html past this line
+
